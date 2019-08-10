212f235010d88db701685b43c2ac99598db65f80
xiaoqi <308805548@qq.com>
2019-08-10 20:13:08 +0800

    初步完成所有功能

--- a/src/lib/common.js
+++
@@ -1,3 +0,0 @@
-console.log("公共部分");
-console.log("公共部分");
-console.log("公共部分");
--- a/src/views/about/about.js
+++
@@ -1,6 +0,0 @@
-import '../../components/header/header.js'
-import './about.less';
-import '../../style/styles.less';
-
-
-
